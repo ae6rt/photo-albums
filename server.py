700ed165bbdf40438a7236a90304e872bcdf4068
Mark S. Petrovic <mspetrovic@gmail.com>
2013-11-24 16:47:39 -0800

    add angular.js and routes for js/css

--- server.py
+++ server.py
@@ -1,34 +1,50 @@
-from bottle import Bottle, run, request, response, abort, static_file,debug
-
 import os
 import json
+
+from bottle import Bottle, run, response, static_file
 
 app = Bottle()
 
 prefix = "./albums"
 
+
 @app.get('/')
 def index():
     return static_file("index.html", root=".")
 
+
 @app.get('/albums')
 def albumlist():
-   return album(None)
+    return album(None)
+
 
 @app.get('/albums/<name>')
 def album(name):
     if name is None:
-       dir = prefix
+        dir = prefix
     else:
-       dir = "%s/%s" % (prefix, name)
+        dir = "%s/%s" % (prefix, name)
     listing = os.listdir(dir)
 
     response.content_type = 'application/json'
     return json.dumps(sorted(listing), indent=3)
 
+
 @app.get('/albums/<name>/<img>')
 def image(name, img):
     filepath = "%s/%s" % (name, img)
     return static_file(filepath, root=prefix)
+
+
+@app.get('/<filename:re:.*\.js>')
+def javascripts(filename):
+    print "here js %s" % filename
+    return static_file(filename, root='static')
+
+
+@app.get('/<filename:re:.*\.css>')
+def stylesheets(filename):
+    return static_file(filename, root='static')
+
 
 run(app, host='localhost', port=8080, debug=True)
