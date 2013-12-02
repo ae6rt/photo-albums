1bb21d0720a88e2ae9a4b90baa01992f289bb979
Mark Petrovic <mspetrovic@gmail.com>
2013-12-02 07:26:55 -0800

    implement inline album title editing

--- static/app/directives.js
+++ static/app/directives.js
@@ -26,17 +26,11 @@ albumApp.directive('inlineAlbumTitleEdit', function () {
         templateUrl: 'partials/albumTitleEdit.html',
         scope: {
             model: '=',
-            album: "=",
-            imageName: "="
+            album: "="
         },
-        controller: ['$scope', '$http', function ($scope, $http) {
-            $scope.update_caption = function () {
-                $http.put("photo/" + $scope.album + "/" + $scope.imageName + "/metadata", {caption: $scope.model})
-                    .success(function (data, status, headers, config) {
-                    })
-                    .error(function (data, status, headers, config) {
-                        console.log("photo caption update failed with status: " + status);
-                    });
+        controller: ['$scope', 'AlbumMetaUpdateService', function ($scope, AlbumMetaUpdateService) {
+            $scope.update_album_title = function () {
+                AlbumMetaUpdateService.update({name: $scope.album, description: $scope.model});
             }
         }]
     };
