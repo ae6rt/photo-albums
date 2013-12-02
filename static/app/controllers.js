748547223fbffa7d41bc1df1390a6e01122bc1f8
Mark Petrovic <mspetrovic@gmail.com>
2013-12-02 01:34:31 -0800

    move directive impl into js/directives.js

--- static/app/controllers.js
+++ static/app/controllers.js
@@ -20,28 +20,6 @@ albumApp.factory('AlbumMetaUpdateService', ['$http', function ($http) {
         }}
 }]);
 
-albumApp.directive('inlineEdit', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'partials/componentTpl.html',
-        scope: {
-            model: '=',
-            album: "=",
-            imageName: "="
-        },
-        controller: ['$scope', '$http', function ($scope, $http) {
-            $scope.update_caption = function () {
-                $http.put("photo/" + $scope.album + "/" + $scope.imageName + "/metadata", {caption: $scope.model})
-                    .success(function (data, status, headers, config) {
-                    })
-                    .error(function (data, status, headers, config) {
-                        console.log("photo caption update failed with status: " + status);
-                    });
-            }
-        }]
-    };
-});
-
 albumApp.controller('AlbumController', function ($scope, $http, $modal, $log) {
 
     $scope.image_names_by_album = [];
